b4fe50364faefedd3a5a6ea5801d269717fa4940
Jenya226 <137316837+Jenya226@users.noreply.github.com>
2024-09-15 15:25:05 +0300

    Updated main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ except:
      print("Backyard is not launched")
 
 user_message=None
-chat_generation_time=25 #seconds
+chat_generation_time = 5 #seconds
 
 def get_location(image):
     try:
@@ -46,22 +46,41 @@ def write_a_message(user_message):
     save_json_chat()
     bot.delete_message(user_message.chat.id, render_message)
     
-bot=tb.TeleBot('blank') #enter token here
+bot=tb.TeleBot('7431833180:AAE6aE2xg9EMbyLMFUhPRX3huO9k_-_PLS0') #enter token here
 
 #Start 
 
-@bot.message_handler(commands=['start']) #load bot into memorry
+@bot.message_handler(commands=['start']) #START command
 def start(message):
-     bot.send_message(message.chat.id,"Hey, I am working on the first message from the bot rn, you can start messaging bot now")
+     bot.send_message(message.chat.id,"Hey, I am working on the first message from the bot rn. \nYou can start messaging bot now. \nUse /help to set render time (5 by default) and pre-load bot")
+
+@bot.message_handler(commands=['set_render_time'])
+def start(message):
+     bot.send_message(message.chat.id,"Type in the new render time:")
+     bot.register_next_step_handler(message,set_render_time)
+
+def set_render_time(message):
+    if int(message.text)>0:
+        global chat_generation_time 
+        chat_generation_time = int(message.text)
+        bot.send_message(message.chat.id,"The render time is set to "+str(chat_generation_time)+" seconds")
+    else:
+        bot.send_message(message.chat.id,"Invalid render time")
+          
+@bot.message_handler(commands=['help']) #HELP
+def start(message): 
+     bot.send_message(message.chat.id,"You can:\n/load_bot\n/set_render_time")   
+
 
 @bot.message_handler(commands=['load_bot']) #load bot into memorry
 def load_bot(message):
-     render_message=bot.send_message(message.chat.id,"Please wait for "+str((chat_generation_time*chat_generation_time)/2)+" seconds, loading bot into memory. This will take arround 5 minutes").message_id
+     render_message=bot.send_message(message.chat.id,"Please wait for "+str((chat_generation_time*5))+" seconds, loading bot into memory.").message_id
      pg.click(get_location('TypeMessage.png'))
-     pg.typewrite('.')
+     pg.typewrite('hi')
      pg.press('Enter')
-     time.sleep(chat_generation_time*3)
+     time.sleep(chat_generation_time*5)
      pg.click(get_location('Undo.png'))
+     pg.press('Backspace')
      pg.press('Backspace')
      bot.delete_message(message.chat.id, render_message)
      bot.send_message(message.chat.id,"Completed!")
@@ -72,16 +91,20 @@ def load_bot(message):
 @bot.callback_query_handler(func=lambda call:True) #commands continue
 def next(message):
      pg.click(get_location('continue.png'))
-     time.sleep(chat_generation_time*0.7)
+     time.sleep(chat_generation_time*0.5)
      pg.click(get_location('TypeMessage.png'))
      save_json_chat()
      with open("1.json","r", encoding='utf-8') as f: #oppening json and printing out the last bot's responce
-            data = json.load(f)
+          data = json.load(f)
      markup=types.InlineKeyboardMarkup(row_width=1)
      continue_button=types.InlineKeyboardButton('Continue', callback_data='/continue')
      markup.add(continue_button)
-     bot.edit_message_text(chat_id=current_message_from_bot.chat.id, text=str(data['chat']['ChatItems'][-1]['output']), message_id=current_message_from_bot.message_id,reply_markup=markup)
+     try:
+          bot.edit_message_text(chat_id=current_message_from_bot.chat.id, text=str(data['chat']['ChatItems'][-1]['output']), message_id=current_message_from_bot.message_id,reply_markup=markup)
+     except:
+          bot.send_message(chat_id=current_message_from_bot.chat.id,text="Empty responce from the model")
      f.close
+
     
 
 
